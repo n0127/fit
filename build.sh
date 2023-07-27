ae613a8395f1b7d058a2052be4e981cf07637ae0
ness <ness@nescom>
2023-07-27 16:29:24 +0800

    d

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ git branch | grep -v 'main' | xargs git branch -D
 git branch -r | grep -v 'main' | sed 's/origin\///' | xargs -I {} git push origin --delete {}
 git add . 
 git commit -m 'd'
-version=eval(grep -o '"version": "[^"]*' | grep -o '[^"]*$')
+version=`grep -o '"version": "[^"]*' | grep -o '[^"]*$'`
+echo $version
 git push origin main
 git checkout $version
 git add . 
