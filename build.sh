77e46de494cf1dd5b4ebc25a6e444c4dd9326926
ness <ness@nescom>
2023-07-27 16:25:36 +0800

    d

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ npm publish
 
 git checkout main
 git branch | grep -v 'main' | xargs git branch -D
+git branch -r | grep -v 'main' | sed 's/origin\///' | xargs -I {} git push origin --delete {}
 git add . 
 git commit -m 'd'
 version=`grep -o '"version": "[^"]*' | grep -o '[^"]*$'`
