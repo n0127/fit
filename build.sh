ac405675fef1a1f2d1c63b2b30ce942c342ca695
ness <ness@nescom>
2023-07-27 16:24:17 +0800

    d

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@ source /etc/profile
 cd /Users/ness/Documents/fit-main
 # npm unpublish gfit --force
 npm publish 
+
+
+git checkout main
+git branch | grep -v 'main' | xargs git branch -D
 git add . 
 git commit -m 'd'
+version=`grep -o '"version": "[^"]*' | grep -o '[^"]*$'`
 git push
+git checkout $version
+git add . 
+git commit -m 'd'
+git push origin $version
