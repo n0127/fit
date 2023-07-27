6fa3d1685b9bb955ee46965263f3b6843a27495a
ness <ness@nescom>
2023-07-27 16:37:16 +0800

    d

--- build.sh
+++ build.sh
@@ -3,17 +3,6 @@ source /etc/profile
 cd /Users/ness/Documents/fit-main
 # npm unpublish gfit --force
 npm publish 
-
-
-git checkout main
-git branch | grep -v 'main' | xargs git branch -D
-git branch -r | grep -v 'main' | sed 's/origin\///' | xargs -I {} git push origin --delete {}
 git add . 
 git commit -m 'd'
-version=`cat package.json|grep -o '"version": "[^"]*' | grep -o '[^"]*$'`
-echo $version
-git push origin main
-git checkout $version
-git add . 
-git commit -m 'd'
-git push origin $version
+git push
